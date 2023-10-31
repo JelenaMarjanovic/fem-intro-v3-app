c67f5d2a3b9227e363830d79e5676b6321049bf0
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2023-10-31 23:24:43 +0100

    Server Components - Fetch data and refactor home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
+const getData = async () => {
+  await new Promise(res => setTimeout(() => res(0), 2000))
+  return { data: [1, 2, 3] }
+}
+
+const Home = async() => {
+  const data = await getData()
+  console.log('data', data)
+
   return <div>Home Page</div>
 }
+
+export default Home
