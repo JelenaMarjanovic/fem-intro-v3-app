6a61191ea4edbae5c5062d80f86d5451d7646e31
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2023-11-02 00:01:37 +0100

    Fetch Data with Prisma and display - Todo setup

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -1,5 +1,22 @@
-const TodosPage = () => {
-  return <div>ToDo's Page</div>
+import TodoList from '@/components/TodoList'
+import db from '@/utils/db'
+
+const getData = async () => {
+  const todos = await db.todo.findMany({})
+
+  console.log('todos', todos)
+
+  return todos
+}
+
+const TodosPage = async () => {
+  const todos = await getData()
+
+  return (
+    <div>
+      <TodoList todos={todos} />
+    </div>
+  )
 }
 
 export default TodosPage
