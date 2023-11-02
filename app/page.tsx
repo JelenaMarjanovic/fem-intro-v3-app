24977b521acd7a9c3aab1d571b73a9b21f7e7219
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2023-11-02 22:14:21 +0100

    Form Handling with Server Actions

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,5 @@
-import NewTodoForm from '@/components/NewTodoForm'
-
-const getData = async () => {
-  await new Promise(res => setTimeout(() => res(0), 2000))
-  return { data: [1, 2, 3] }
-}
-
 const Home = async () => {
-  const data = await getData()
-  console.log('data', data)
-
-  return (
-    <div>
-      <NewTodoForm />
-    </div>
-  )
+  return <div>Home</div>
 }
 
 export default Home
